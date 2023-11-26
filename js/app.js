a083c4f04ee39b4cb7d5e985284af531cbd8996b
anvarbekov <mustafoanvarbekov24@gmail.com>
2023-11-26 15:57:25 +0500

    style changes

--- js/app.js
+++ js/app.js
@@ -49,6 +49,39 @@ function nowDate() {
   return `${hours}:${minute}:${seconds}, ${date}.${month}.${years}`;
 }
 setInterval(nowDate, 1000);
+
+/* ------------------------ Week function ------------------------ */
+const now = new Date();
+day = now.getDay();
+const week = document.getElementById("week");
+switch (day) {
+  case 0:
+    day = "Yakshanba";
+    break;
+  case 1:
+    day = "Dushanba";
+    break;
+  case 2:
+    day = "Seshanba";
+    break;
+  case 3:
+    day = "Chorshanba";
+    break;
+  case 4:
+    day = "Payshanba";
+    break;
+  case 5:
+    day = "Juma";
+    break;
+  case 6:
+    day = "Shanba";
+    break;
+  default:
+    day = "Days of the week";
+    break;
+}
+week.textContent = day;
+
 /* ------------------------ Date function ------------------------ */
 
 // error message function
